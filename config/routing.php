a15260f5ddaf14852ef2df47053a930ac818887c
Valentin Commenge <txreplay@gmail.com>
2016-11-24 15:42:19 +0100

    verifying route exists

--- config/routing.php
+++ config/routing.php
@@ -2,15 +2,15 @@
 
 // List all routes
 $existing_routes = [
-    'homepage'
+    'homepage', 'login', 'signup'
 ];
 
 // List all actions
 $existing_actions = [
-
+    'action_homepage', 'action_login', 'action_logout', 'action_signup'
 ];
 
-if (!empty($_GET['action'])) {
+if (!empty($_GET['action']) && array_search($_GET['action'], $existing_routes)) {
     $action = $_GET['action'];
 }
 
