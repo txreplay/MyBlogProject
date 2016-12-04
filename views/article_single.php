e8af0f07717d40dafa1807076d5aeadb0323bc13
Valentin Commenge <txreplay@gmail.com>
2016-12-04 16:07:31 +0100

    design for single_article

--- views/article_single.php
+++ views/article_single.php
@@ -4,7 +4,15 @@ if (isset($message)) {
 }
 ?>
 
-<h2><?=$article['article_title']?></h2>
-<p>Créé par <?=$article['user_username']?>, le <?=$article['article_created']?></p>
+<article>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <h2 class="section-heading"><?=$article['article_title']?></h2>
+                <p class="post-meta">Posté par <a href="index.php?action=profile_show&id=<?=$article['user_id']?>"><?=$article['user_username']?></a> le  <?=$article['article_created']?></p>
 
-<p><?=$article['article_content']?></p>
+                <p><?=$article['article_content']?></p>
+            </div>
+        </div>
+    </div>
+</article>
