8327d8601472315faf0783fa8cf4fe44ee81a48c
Valentin Commenge <txreplay@gmail.com>
2016-12-04 16:32:44 +0100

    front for new article

--- controller/action_article_new.php
+++ controller/action_article_new.php
@@ -5,7 +5,7 @@ require_once 'model/article.php';
 $errors = check_save();
 
 if (empty($errors)) {
-    article_save($_POST['title'], $_POST['content'], $user['id'], 1, $_POST['status']);
+    article_save($_POST['title'], $_POST['chapeau'], $_POST['content'], $user['id'], 1, $_POST['status']);
 
     $message = [
         'type' => 'success',
@@ -23,6 +23,10 @@ function check_save()
 
     if(empty($_POST['title'])) {
         $errors['title'] = "Titre obligatoire";
+    }
+
+    if(empty($_POST['chapeau'])) {
+        $errors['chapeau'] = "Chapeau obligatoire";
     }
 
     if(empty($_POST['content'])) {
--- views/article_new.php
+++ views/article_new.php
@@ -1,41 +1,67 @@
-<h2>Écrire un article</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            <h2>Écrire un article</h2>
 
-<form action="index.php?action=action_article_new" method="post">
-    <div>
-        <label for="title">Titre de l'article</label>
-        <input type="text" name="title" <?php if (isset($_POST['title'])) { echo 'value="'.$_POST['title'].'"'; } ?> id="title" placeholder="Titre de votre article">
+            <form action="index.php?action=action_article_new" method="post">
+                <div class="row control-group">
+                    <div class="form-group col-xs-12 floating-label-form-group controls">
+                        <label for="title">Titre de l'article</label>
+                        <input type="text" class="form-control" name="title" <?php if (isset($_POST['title'])) { echo 'value="'.$_POST['title'].'"'; } ?> id="title" placeholder="Titre de votre article">
+                    </div>
+                </div>
+                <div class="row control-group">
+                    <div class="form-group col-xs-12 floating-label-form-group controls">
+                        <label for="chapeau">Chapeau de l'article</label>
+                        <textarea name="chapeau" class="form-control" cols="30" rows="10" id="chapeau" placeholder="Chapeau de l'article"><?php if (isset($_POST['chapeau'])) { echo $_POST['chapeau']; } ?></textarea>
+                    </div>
+                </div>
+                <div class="row control-group">
+                    <div class="form-group col-xs-12 floating-label-form-group controls">
+                        <label for="content">Contenu de l'article</label>
+                        <textarea name="content" class="form-control" id="" cols="30" rows="10" id="content" placeholder="Contenu de l'article"><?php if (isset($_POST['content'])) { echo $_POST['content']; } ?></textarea>
+                        </div>
+                </div>
+                <div class="row control-group">
+                    <div class="form-group col-xs-12 floating-label-form-group controls">
+                        <label for="status">Statut de l'article</label>
+                        <select name="status" id="status">
+                            <option value="0">En préparation</option>
+                            <option value="1">Publié</option>
+                            <option value="2">Non-publié</option>
+                            <option value="3">Supprimé</option>
+                        </select>
+                    </div>
+                </div>
+                <br>
+                <div id="success"></div>
+                <div class="row">
+                    <div class="form-group col-xs-12">
+                        <input type="submit" class="btn btn-default" value="Poster">
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
+</div>
 
-    <div>
-        <label for="chapeau">Chapeau de l'article</label>
-        <textarea name="chapeau" id="" cols="30" rows="10" id="chapeau" placeholder="Chapeau de l'article"><?php if (isset($_POST['chapeau'])) { echo 'value="'.$_POST['chapeau'].'"'; } ?></textarea>
-    </div>
-
-    <div>
-        <label for="content">Contenu de l'article</label>
-        <textarea name="content" id="" cols="30" rows="10" id="content" placeholder="Contenu de l'article"><?php if (isset($_POST['content'])) { echo 'value="'.$_POST['content'].'"'; } ?></textarea>
-    </div>
-
-    <div>
-        <label for="status">Statut de l'article</label>
-        <select name="status" id="status">
-            <option value="0">En préparation</option>
-            <option value="1">Publié</option>
-            <option value="2">Non-publié</option>
-            <option value="3">Supprimé</option>
-        </select>
-    </div>
-    <div>
-        <input type="submit" value="Poster">
-    </div>
-
-    <ul>
+<script>
+    (function() {
         <?php
         if (isset($errors)) {
-            foreach ($errors as $key => $error) {
-                echo '<li>'.$key.': '.$error.'</li>';
-            }
+        foreach ($errors as $key => $error):
+        ?>
+        iziToast.error({
+            title: 'Erreur',
+            message: "<?php echo $error ?>",
+            position: 'topRight',
+            layout: 2,
+            color: 'red',
+            timeout: 10000
+        });
+        <?php
+        endforeach;
         }
         ?>
-    </ul>
-</form>
+    })();
+</script>
